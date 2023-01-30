3b798f99c0244ef0ee852790457d7bc099d43576
Scytale <federicoboainain@gmail.com>
2023-01-30 17:18:22 -0300

    Agregando formBuilder y algunos nuevos styles

--- src/app/component/calculador/calculador.component.ts
+++ src/app/component/calculador/calculador.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calculador',
@@ -8,22 +8,50 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CalculadorComponent implements OnInit {
 
-  constructor() { }
-
   calculadorForm:FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.calculadorForm = this.formBuilder.group(
+      {
+        muro: ['', Validators.required],
+        tipoPanel: ['', Validators.required],
+        metrosLineales: ['', Validators.required],
+        pxPanel: ['', Validators.required],
+        pxMO: ['', Validators.required]
+      }
+    )
+   }
+
+  
+
   ngOnInit(): void {
-    this.calculadorForm = new FormGroup({
-      muro: new FormControl(null),
-      tipoPanel: new FormControl(null),
-      metrosLineales: new FormControl(null),
-      pxPanel: new FormControl(null),
-      pxMO: new FormControl(null)
-    })
+    // this.calculadorForm = new FormGroup({
+    //   muro: new FormControl(null, Validators.required),
+    //   tipoPanel: new FormControl(null, Validators.required),
+    //   metrosLineales: new FormControl(null, Validators.required),
+    //   pxPanel: new FormControl(null, Validators.required),
+    //   pxMO: new FormControl(null, Validators.required)
+    // })
   }
 
   onSubmit(){
-    console.log(this.calculadorForm);
+    console.log(this.calculadorForm.value);
+  }
+
+  get muro(){
+    return this.calculadorForm.get('muro');
+  }
+  get tipoPanel(){
+    return this.calculadorForm.get('tipoPanel');
+  }
+  get metrosLineales(){
+    return this.calculadorForm.get('metrosLineales');
+  }
+  get pxPanel(){
+    return this.calculadorForm.get('pxPanel');
+  }
+  get pxMO(){
+    return this.calculadorForm.get('pxMO');
   }
 
 
